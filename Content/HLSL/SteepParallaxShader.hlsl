809d76d6c654de1144b4ae7312aa9000b9097768
Saad Khattak <saadrustam@gmail.com>
2018-10-02 00:29:32 -0400

    Steep parallax mapping (without interpolation)

--- Content/HLSL/SteepParallaxShader.hlsl
+++ Content/HLSL/SteepParallaxShader.hlsl
@@ -4,17 +4,14 @@ float2 p = viewDir.xy * heightScale;
 float2 deltaTexCoords = p / numLayers;
 
 float2 currentTexCoords = texCoords;
-float  currentDepthMapValue = Texture2DSample(depthMap, depthMapSampler, currentTexCoords).r;
+float  currentDepthMapValue = 1.0 - Texture2DSample(depthMap, depthMapSampler, currentTexCoords).r;
 
 float currentLayerDepth = 0.0;
-
-float2 dx = ddx(texCoords);
-float2 dy = ddy(texCoords);
 
 while (currentLayerDepth < currentDepthMapValue)
 {
     currentTexCoords -= deltaTexCoords;
-    currentDepthMapValue = depthMap.SampleGrad(depthMapSampler, currentTexCoords, dx, dy).r;
+    currentDepthMapValue = 1.0 - depthMap.SampleGrad(depthMapSampler, currentTexCoords, ddx, ddy).r;
     currentLayerDepth += layerDepth;
 }
 
